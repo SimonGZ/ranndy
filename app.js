e17ff464f891d733371d594560da15bbcdd60ed4
Simon Ganz <simong@gmail.com>
2014-02-18 21:47:42 -0800

    All the basic api functionality is in place. Clean up and refactoring needs to happen. Maybe a test suite too?

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var async = require('async');
 
 
 // Sample Data
@@ -42,7 +43,8 @@ app.get('/surnames', function(req, res) {
 			return console.error('Error fetching client from pool', err);
 		}
 
-		getSurnames(client, res);
+		getSurnames(client, req.query, function(results) { return res.json(results) });
+
 		done();
 	});
 });
@@ -53,9 +55,33 @@ app.get('/firstnames', function(req, res) {
 			return console.error('Error fetching client from pool', err);
 		}
 
-		getFirstNames(client, function(results) { res.json(results) });
+		getFirstNames(client, req.query, function(results) { return res.json(results) });
 
 		done();
+	});
+});
+
+app.get('/names/:gender', function(req, res) {
+	pg.connect(conString, function(err, client, done) {
+		if (err) {
+			return console.error('Error fetching client from pool', err);
+		}
+
+		async.parallel(
+		{
+			firstnames: function(callback) {
+				getFirstNames(client, req.query, function(results) { callback(null, results) });
+			},
+			surnames: function(callback) {
+				getSurnames(client, req.query, function(results) { callback(null, results) });
+			}
+		},
+		function(err, results) {
+			done();
+			console.log(req.params);
+			res.json(results);
+		}
+		);
 	});
 });
 
@@ -65,31 +91,137 @@ app.get('/names', function(req, res) {
 			return console.error('Error fetching client from pool', err);
 		}
 
-		getFirstNames(client, function(x) { res.json(results) });
-
-		done();
+		async.parallel(
+		{
+			firstnames: function(callback) {
+				getFirstNames(client, req.query, function(results) { callback(null, results) });
+			},
+			surnames: function(callback) {
+				getSurnames(client, req.query, function(results) { callback(null, results) });
+			}
+		},
+		function(err, results) {
+			done();
+			res.json(results);
+		}
+		);
 	});
 });
 
-function getFirstNames(client, fn) {
-	var querystring = 'SELECT * from firstnames ORDER BY random() LIMIT 10'
+function capitalizeFirstLetter(string)
+{
+	string = string.toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+String.prototype.splice = function( idx, rem, s ) {
+    return (this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
+};
+
+function andOrWhere(querystring) {
+	if (querystring.indexOf("WHERE") === -1 ) {
+		return " WHERE ";
+	}
+	else {
+		return " AND ";
+	}
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function containsRace(string) {
+	if (string.indexOf("pctwhite") > -1 || string.indexOf("pctblack") > -1 || string.indexOf("pctasian") > -1 || string.indexOf("pctnative") > -1 || string.indexOf("pcthispanic") > -1)  {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function updateQueryWithLimit(string, limit) {
+	if (isNumber(limit)) {
+		if (limit <= 100) {
+			return string.replace("LIMIT 10", "LIMIT " + limit);
+		}
+		else {
+			return string;
+		}
+	}
+}
+
+function getFirstNames(client, params, callback) {
+	
+	var querystring = "SELECT name, gender, rank, frequency from firstnames ORDER BY random() LIMIT 10";
+
+	if (params['gender']) {
+		if (params['gender'].toLowerCase() == 'female' || params['gender'].toLowerCase() == 'male') {
+			position = querystring.indexOf("ORDER BY");
+			querystring = querystring.splice(position, 0, andOrWhere(querystring) + "gender = '" + capitalizeFirstLetter(params['gender']) +"'");
+		}
+	}
+
+	if (params['fRank']) {
+		if (isNumber(params['fRank'])) {
+			position = querystring.indexOf("ORDER BY");
+			querystring = querystring.splice(position, 0, andOrWhere(querystring) + "rank < '" + params['fRank'] +"'");	
+		}
+	}
+
+	if (params['limit']) {
+		querystring = updateQueryWithLimit(querystring, params['limit']);		
+	}
+
 	client.query(querystring, function(err, result) {
 
 		if (err) {
+			callback(err);
 			return console.error('Error running query', err);
 		}
-		fn(result.rows);
+		callback(result.rows);
 	});
 };
 
-var getSurnames = function(client, res) {
-	var querystring = 'SELECT * from surnames ORDER BY random() LIMIT 10'
+var getSurnames = function(client, params, callback) {
+	var querystring = 'SELECT name, rank, frequency, pctwhite, pctblack, pctasian, pctnative, pcthispanic from surnames ORDER BY random() LIMIT 10'
+
+	if (params['sRank']) {
+		if (isNumber(params['sRank'])) {
+			position = querystring.indexOf("ORDER BY");
+			querystring = querystring.splice(position, 0, andOrWhere(querystring) + "rank < '" + params['sRank'] +"'");	
+		}
+	}
+
+	if (params['race']) {
+		var percent, race;
+		if (containsRace(params['race']) && params['race'].indexOf(",") > -1) {
+			race = params['race'].split(",")[0];
+			percent = params['race'].split(",")[1];
+			if (isNumber(percent)) {
+				if (percent > 100 || percent < 0) {
+					percent = 0;
+				}
+			}
+			else {
+				percent = 0;
+			}
+			position = querystring.indexOf("ORDER BY");
+			querystring = querystring.splice(position, 0, andOrWhere(querystring) + race + " > " + percent);			
+		}		
+	}
+
+	if (params['limit']) {
+		querystring = updateQueryWithLimit(querystring, params['limit']);		
+	}
+
 	client.query(querystring, function(err, result) {
 
 		if (err) {
+			callback(err);
 			return console.error('Error running query', err);
 		}
-		res.json(result.rows);
+		callback(result.rows);
 	});
 };
 
