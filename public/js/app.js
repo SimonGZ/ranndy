2f26ae1f681609c1c994a5be83952f5595011615
Simon Ganz <simong@gmail.com>
2014-03-03 23:04:01 -0800

    Got some very basic controls and lock functions working.

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,15 @@ var randomNameApp = angular.module('randomNameApp', [
   'randomNameControllers',
   'ngAnimate'
 ]);
+
+randomNameApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'js/partials/full-name-list.html',
+        controller: 'NameListCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,9 +6,59 @@ randomNameControllers.controller('NameListCtrl', function ($scope, $http) {
 		$scope.names = data;
 	});
 
-	$scope.generate = function() {
-		$http.get('api/names').success(function(data) {
+	$scope.generate = function(query) {
+		$http.get('api/names', {params: query}).success(function(data) {
+			if ($scope.selected_first) {
+				for (var i = data.length - 1; i >= 0; i--) {
+					$scope.covered_firsts[i] = data[i].firstname.name
+					data[i].firstname.name = $scope.selected_first;
+				};
+			}
+			if ($scope.selected_last) {
+				for (var i = data.length - 1; i >= 0; i--) {
+					$scope.covered_lasts[i] = data[i].surname.name
+					data[i].surname.name = $scope.selected_last;
+				};
+			}
 			$scope.names = data;
 		});
 	};
+
+	$scope.lockFirst = function(name) {
+		if ($scope.selected_first === name) {
+			$scope.selected_first = null;
+			for (var i = $scope.names.length - 1; i >= 0; i--) {
+				$scope.names[i].firstname.name = $scope.covered_firsts[i]
+			};	
+		}
+		else {
+			$scope.selected_first = name;
+			for (var i = $scope.names.length - 1; i >= 0; i--) {
+				$scope.covered_firsts[i] = $scope.names[i].firstname.name
+				$scope.names[i].firstname.name = $scope.selected_first;
+			};	
+		}
+		
+	};
+
+	$scope.lockLast = function(name) {
+		if ($scope.selected_last === name) {
+			$scope.selected_last = null;
+			for (var i = $scope.names.length - 1; i >= 0; i--) {
+				$scope.names[i].surname.name = $scope.covered_lasts[i]
+			};	
+		}
+		else {
+			$scope.selected_last = name;
+			for (var i = $scope.names.length - 1; i >= 0; i--) {
+				$scope.covered_lasts[i] = $scope.names[i].surname.name
+				$scope.names[i].surname.name = $scope.selected_last;
+			};	
+		}
+		
+	};
+
+	$scope.genders = [];
+	$scope.covered_firsts = [];
+	$scope.covered_lasts = [];
 });
