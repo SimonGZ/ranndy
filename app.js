9aacae028d1fa9201ec0c8af650242efdccaacee
Simon Ganz <simong@gmail.com>
2014-04-20 10:40:51 -0700

    Hello World and Serving Static Files

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var express = require("express")
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
